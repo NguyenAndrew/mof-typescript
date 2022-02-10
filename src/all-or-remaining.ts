2e7c3f34cc6eea0421074622c83c6577d4e5410f
NguyenAndrew <anguyen29@wisc.edu>
2022-02-09 23:44:11 -0600

    Add mof and nof enums for AllOrRemaining and FirstOrLast

---
+++ src/all-or-remaining.ts
@@ -0,0 +1,4 @@
+export enum AllOrRemaining {
+  ALL,
+  REMAINING,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
+export { AllOrRemaining } from "./all-or-remaining";
+export { FirstOrLast } from "./first-or-last";
 export { sayHello, sayGoodbye } from "./hello-world";
