1faf16444572210f6967b07c855adcf0a21a60b5
NguyenAndrew <anguyen29@wisc.edu>
2022-02-10 00:18:31 -0600

    Start creating Mof

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { AllOrRemaining } from "./all-or-remaining";
 export { FirstOrLast } from "./first-or-last";
+export { Mof } from "./mof";
 export { sayHello, sayGoodbye } from "./hello-world";
---
+++ src/mof.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { FirstOrLast } from "./first-or-last";
+import { AllOrRemaining } from "./all-or-remaining";
+
+export class Mof {
+  public static ALL = AllOrRemaining.ALL;
+  public static REMAINING = AllOrRemaining.REMAINING;
+  public static FIRST = FirstOrLast.FIRST;
+  public static LAST = FirstOrLast.LAST;
+
+  /**
+   * Runs ALL or REMAINING whens.
+   *
+   * @param aor - ALL or REMAINING enum.
+   * @throws Not calling with ALL or REMAINING enum.
+   *
+   */
+  public when(aor: AllOrRemaining): void {
+    console.log("Unimplemented");
+  }
+
+  public static Builder = class {};
+}
