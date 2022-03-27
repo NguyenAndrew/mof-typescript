3fc15986dd5ce994244276519d73d19172e7e420
NguyenAndrew <anguyen29@wisc.edu>
2022-03-27 18:41:37 -0500

    Test withTwo and withThree mocks for builder

--- src/mof.test.ts
+++ src/mof.test.ts
@@ -5,9 +5,20 @@ describe('MofTest', () => {
     const mock1 = new class {
         callExample = jest.fn()
     };
+    const mock2 = new class {
+        callExample = jest.fn()
+    };
+    const mock3 = new class {
+        callExample = jest.fn()
+    };
 
     const when1 = jest.fn();
+    const when2 = jest.fn();
+    const when3 = jest.fn();
+
     const verify1 = jest.fn();
+    const verify2 = jest.fn();
+    const verify3 = jest.fn();
 
     describe('Builder', () => {
 
@@ -51,6 +62,41 @@ describe('MofTest', () => {
                     verify1
                 )
                 .enableVerifyNoInteractions(verifyNoInteractionLambda)
+                .build();
+        });
+
+        test('withTwoMocks_success', () => {
+            new Mof.Builder()
+                .add(
+                    mock1,
+                    when1,
+                    verify1
+                )
+                .add(
+                    mock2,
+                    when2,
+                    verify2
+                )
+                .build();
+        });
+
+        test('withThreeMocks_success', () => {
+            new Mof.Builder()
+                .add(
+                    mock1,
+                    when1,
+                    verify1
+                )
+                .add(
+                    mock2,
+                    when2,
+                    verify2
+                )
+                .add(
+                    mock3,
+                    when3,
+                    verify3
+                )
                 .build();
         });
     });
